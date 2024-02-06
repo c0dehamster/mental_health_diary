25a1665d3d5e4dc843723c11e669c3da9f118e18
Sergej Smelov <sergeysmelow@gmail.com>
2024-02-06 22:34:59 +0500

    Add the option to create a mood record

--- lib/components/mood_picker.dart
+++ lib/components/mood_picker.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hive/hive.dart';
+import 'package:mental_health_diary/models/mood_record.dart';
 import 'package:mental_health_diary/theme/mood_spectre.dart';
 
 enum MoodValue { value1, value2 }
@@ -12,7 +14,33 @@ class MoodPicker extends StatefulWidget {
 }
 
 class _MoodPickerState extends State<MoodPicker> {
+  late final Box box;
+
   int? _moodValue;
+
+  // Create a new mood record entry
+
+  _addRecord() async {
+    if (_moodValue != null) {
+      // Only try to create a new record if a value is chosen
+      var newRecord = MoodRecord(
+        value: _moodValue!,
+        timestamp: DateTime.timestamp(),
+      );
+
+      box.add(newRecord);
+      print("New entry added");
+      _moodValue = null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Get reference to the mood records box
+    box = Hive.box("records");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,40 +56,53 @@ class _MoodPickerState extends State<MoodPicker> {
         .map((emoji) => SvgPicture.asset(emoji, width: 24, height: 24))
         .toList();
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        const Text("How would you rate your mood?"),
-
-        const SizedBox(height: 32),
-
-        // Emoji labels
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: emojis,
-        ),
-
-        // Mood select checkbox group
-        Row(
-          children: List<Widget>.generate(moodSpectre.length, (index) {
-            return Expanded(
-              child: Transform.scale(
-                scale: 0.75,
-                child: Radio<int>(
-                    toggleable: true,
-                    fillColor: MaterialStateProperty.all(moodSpectre[index]),
-                    value: index,
-                    groupValue: _moodValue,
-                    onChanged: (value) {
-                      setState(() {
-                        _moodValue = value;
-                      });
-                    }),
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("How would you rate your mood?"),
+              IconButton(
+                onPressed: () {
+                  _addRecord();
+                },
+                icon: const Icon(Icons.add),
               ),
-            );
-          }),
-        ),
-      ],
+            ],
+          ),
+
+          const SizedBox(height: 32),
+
+          // Emoji labels
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: emojis,
+          ),
+
+          // Mood select checkbox group
+          Row(
+            children: List<Widget>.generate(moodSpectre.length, (index) {
+              return Expanded(
+                child: Transform.scale(
+                  scale: 0.75,
+                  child: Radio<int>(
+                      toggleable: true,
+                      fillColor: MaterialStateProperty.all(moodSpectre[index]),
+                      value: index,
+                      groupValue: _moodValue,
+                      onChanged: (value) {
+                        setState(() {
+                          _moodValue = value;
+                        });
+                      }),
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
     );
   }
 }
