f03b1c48e682a6a8fa6d90d6131ebdfb8abe5eeb
Sergej Smelov <109071165+c0dehamster@users.noreply.github.com>
2024-02-07 20:39:40 +0500

    Add the option to delete all records

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:mental_health_diary/components/app_drawer.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
+  // For now, reset just clears all the mood entries
+
   @override
   Widget build(BuildContext context) {
+    final recordsBox = Hive.box("records");
+
+    clearRecords() async {
+      recordsBox.clear();
+      print("All clear");
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.background,
@@ -15,22 +25,9 @@ class SettingsPage extends StatelessWidget {
         titleSpacing: 0,
 
         // Date navigator
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.chevron_left),
-            ),
-            const Text(
-              "Settings",
-              style: TextStyle(fontSize: 14),
-            ), // Replace later with the actual date
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.chevron_right),
-            ),
-          ],
+        title: const Text(
+          "Settings",
+          style: TextStyle(fontSize: 14),
         ),
         actions: [
           Padding(
@@ -44,10 +41,24 @@ class SettingsPage extends StatelessWidget {
       ),
       drawer: const AppDrawer(),
       body: ListView(
-        children: const [
-          Center(
-            child: Text("Settings"),
-          )
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    clearRecords();
+                  },
+                  child: const Text(
+                    "Clear records",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
